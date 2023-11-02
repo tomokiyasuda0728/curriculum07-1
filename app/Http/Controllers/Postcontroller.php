d9b8f19e9371b528cd64ef64bf35d86ee5fefc03
tomokiyasuda0728 <mousealpaca0728@outlook.jp>
2023-11-02 12:00:19 +0900

    ブログ投稿の編集画面作成

--- app/Http/Controllers/Postcontroller.php
+++ app/Http/Controllers/Postcontroller.php
@@ -10,7 +10,7 @@ class Postcontroller extends Controller
 {
        public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
     {
-        return view('posts.index') ->with(['posts' => $post->getPaginateByLimit(1)]);//$postの中身を戻り値にする。
+        return view('posts.index') ->with(['posts' => $post->getPaginateByLimit(3)]);//$postの中身を戻り値にする。
     }
     
         public function show(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
@@ -27,6 +27,18 @@ class Postcontroller extends Controller
     {
        $input = $request['post'];
        $post -> fill($input)->save();
+       return redirect('/posts/' . $post->id);
+    }
+    
+        public function edit(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+    {
+       return view('posts.edit') ->with(['post' => $post]);//$postの中身を戻り値にする。
+    }
+    
+     public function update(Post $post, PostRequest $request)
+    {
+       $input_post = $request['post'];
+       $post -> fill($input_post)->save();
        return redirect('/posts/' . $post->id);
     }
     
