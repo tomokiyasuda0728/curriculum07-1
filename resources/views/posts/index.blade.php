c0372fcbbf4645b88bddef135a8563aabc7e0de1
tomokiyasuda0728 <mousealpaca0728@outlook.jp>
2023-11-03 18:48:16 +0900

    ブログ投稿の削除

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,7 +13,12 @@
             @foreach ($posts as $post)
                 <div class='post'>
                     <h2 class='title'><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
-                    <p class='body'>{{ $post->body }}</p>    
+                    <p class='body'>{{ $post->body }}</p>
+                    <form action="/posts/{{ $post->id }}" id="form_{{$post->id}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{ $post->id}})">削除</button>
+                    </form>
                 </div>
             @endforeach
         </div>
@@ -24,6 +29,17 @@
         <div>
             <a href='/posts/create'>create</a>
         </div>
+        
+        <script>
+            function deletePost(id){
+                'use strict'
+                
+                if (confirm('削除すると復元できません。\n本当に削除しますか？')){
+                    document.getElementById(`form_${id}`).submit();
+                }
+            }
+        </script>
+        
     </body>
 
 </html>
